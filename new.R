3f557099e8b1f55939b951b6c50d54bd7563b46f
Hernan David Torres <hdtorres@esri.co>
2019-04-25 22:54:25 -0500

    lll

--- new.R
+++ new.R
@@ -344,7 +344,87 @@ for(k in 5:21){
   colnames(settasas4)[k] <- paste0(2000+k-3,".tasa")
 }
 
+### Quinto archivo
 
-settasasall <- unique(rbind(settasas1,settasas2, settasas3, settasas4))
+rownames <- getSheetNames("Data/Municipios5.xlsx")
+
+i <- 1
+municipio <- read_excel("Data/Municipios5.xlsx", 
+                        sheet = i, range = "B3:D19", col_names = c("Medicina Legal", "Policía", "Fiscalía")) 
+
+
+municipio <- as.data.frame(t(municipio))
+major <- matrix(ncol= 17, nrow=1, 0)
+fuente <- matrix(ncol= 17, nrow=1, 0)
+
+for(i in  1:17){
+  major[i] <- municipio[order(municipio[,i], decreasing=TRUE)[1],i]
+  fuente[i]<- row.names(municipio)[order(municipio[,i], decreasing=TRUE)[1]]
+}
+
+setconteos <- major
+setfuentes <- fuente
+
+for(i in  1:17){
+  major[i] <- municipio[order(municipio[,i], decreasing=TRUE)[1],i]
+  fuente[i]<- row.names(municipio)[order(municipio[,i], decreasing=TRUE)[1]]
+}
+
+for(i in 2:37){
+  municipio <- read_excel("Data/Municipios5.xlsx", 
+                          sheet = i, range = "B3:D19", col_names = c("Medicina Legal", "Policía", "Fiscalía")) 
+  
+  municipio <- as.data.frame(t(municipio))
+  major <- matrix(ncol= 17, nrow=1, 0)
+  fuente <- matrix(ncol= 17, nrow=1, 0)
+  
+  for(i in  1:17){
+    major[i] <- municipio[order(municipio[,i], decreasing=TRUE)[1],i]
+    fuente[i]<- row.names(municipio)[order(municipio[,i], decreasing=TRUE)[1]]
+  }
+  setconteos <- rbind(setconteos, major)
+  setfuentes <- rbind(setfuentes, fuente)
+  
+}
+row.names(setconteos) <-rownames
+colnames(setconteos) <- c("2002.conteo", "2003.conteo", "2004.conteo", "2005.conteo", "2006.conteo", "2007.conteo", "2008.conteo", "2009.conteo", "2010.conteo", "2011.conteo", "2012.conteo", "2013.conteo", "2014.conteo", "2015.conteo", "2016.conteo", "2017.conteo", "2018.conteo")
+
+row.names(setfuentes) <-rownames
+colnames(setfuentes) <- c("2002.fuente", "2003.fuente", "2004.fuente", "2005.fuente", "2006.fuente", "2007.fuente", "2008.fuente", "2009.fuente", "2010.fuente", "2011.fuente", "2012.fuente", "2013.fuente", "2014.fuente", "2015.fuente", "2016.fuente", "2017.fuente", "2018.fuente")
+
+
+settasas <- matrix(0, ncol = 4, nrow = 37)
+for(i in 1:37){
+  settasas[i,1] <- rownames[i]
+}
+
+
+for(i in t(settasas[,1])){
+  settasas[settasas[,1]== i,2] <- Poblacion$MUNICIPIO[Poblacion$DPMP == i]
+}
+
+
+for(i in t(settasas[,1])){
+  settasas[settasas[,1]== i,3] <- Poblacion$DEPARTAMENTO[Poblacion$DPMP == i]
+}
+
+
+for(i in t(settasas[,1])){
+  settasas[settasas[,1]== i,4] <- paste0(Poblacion$MUNICIPIO[Poblacion$DPMP == i], " - ", Poblacion$DEPARTAMENTO[Poblacion$DPMP == i])
+}
+
+colnames(settasas) <- c("Código", "Municipio", "Departamento", "Municipio - Departamento")
+
+
+settasas5 <- as_tibble(cbind(settasas, matrix(0, ncol=17, nrow=37), setconteos, setfuentes))
+
+for(k in 5:21){
+  for(i in t(settasas5[,1])){
+    settasas5[settasas5[,1]==i,k] <- (as.numeric(settasas5[settasas5[,1]==i, k + 17])/Poblacion[Poblacion$DPMP == i,k+2])*100000
+  }
+  colnames(settasas5)[k] <- paste0(2000+k-3,".tasa")
+  
+}
+settasasall <- unique(rbind(settasas1,settasas2, settasas3, settasas4, settasas5))
 
 write.xlsx(settasasall, file = "Última.xlsx", sheetName = "Tasas y conteos")
